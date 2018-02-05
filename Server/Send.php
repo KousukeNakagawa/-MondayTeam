a61f38f1e0274eb4ba1d901a7134c22f6084e3d5
He Jian <623695056@qq.com>
2018-02-05 15:50:35 +0900

    Update

--- Server/Send.php
+++ Server/Send.php
@@ -1,20 +1,24 @@
 <?php
-$id = $_GET['id'];
-
 # データベース接続
-$sql = 'INSERT INTO Player(id, currentObj, nextObj, score) VALUES($id,0,0,0)'; 
-
+$id = $_GET['id']; #idを取得
 $dbh = new PDO('mysql;dbname=TowerBattle;host=localhost',
 				 'root',
 				 'H@chiouji1');
 				 
-$sth = $dbh->prepare($sql);
-$sth->execute();
+#idを取得 
+$playerid=$id;
 
-# プレイヤーを返す
-$sql = 'SELECT id FROM Player';
+if($id%2==0){
+	$playerid=$playerid-1;
+}
+else{
+	$playerid=$playerid+1;
+}
+
+# プレイヤー番号
+$sql = "SELECT currentObj,nextObj,score FROM Player WHERE id=$playerid";
 $sth = $dbh->prepare($sql); 
-$sth->execute();
+$sth->execute();  //実行
 $result=$sth->fetch(PDO::FETCH_ASSOC);
 
 header('Access-Control-Allow-Origin: *');
